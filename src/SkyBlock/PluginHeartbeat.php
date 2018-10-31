7573570d1aad9df682c63d976182af24755f7d86
Savion <saviondeavault@icloud.com>
2018-10-31 10:14:02 -0400

    Changes :D

---
+++ src/SkyBlock/PluginHeartbeat.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SkyBlock;
+
+use pocketmine\scheduler\Task;
+
+class PluginHeartbeat extends Task {
+
+    /** @var int */
+    private $nextUpdate = 0;
+
+    private $plugin;
+
+    public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+    }
+
+    /**
+     * @return Main
+     */
+    public function getPlugin(): Main{
+        return $this->plugin;
+    }
+
+    public function onRun($currentTick) {
+        $this->nextUpdate++;
+        /** @var Main $owner */
+        if($this->nextUpdate == 120) {
+            $this->nextUpdate = 0;
+            $this->getPlugin()->getIslandManager()->update();
+        }
+        $this->getPlugin()->getInvitationHandler()->tick();
+        $this->getPlugin()->getResetHandler()->tick();
+    }
+
+}
